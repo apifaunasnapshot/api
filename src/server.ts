6c84111acdcb813abced210a849f16c9d66f735b
gastawny <gastawny@gmail.com>
2023-04-20 18:08:34 -0300

    Create connection with database

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express from "express";
+import dataBase from "./config/dbConnect";
+
+dataBase.on("error", console.log.bind(console, "connection error"));
+dataBase.once("open", () => {
+  console.log("successful connection to the database");
+});
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
