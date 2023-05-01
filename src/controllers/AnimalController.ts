4ec11b41a36b5c7b8c0d28ca4b7c60f156716a24
gastawny <gastawny@gmail.com>
2023-05-01 15:32:56 -0300

    Update Controllers

--- src/controllers/AnimalController.ts
+++ src/controllers/AnimalController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
+import Teacher from "../models/Teacher";
 import Animal from "../models/Animal";
 
 class AnimalController {
   static async getAnimals(request: Request, response: Response) {
     try {
-      const animals = await Animal.find();
+      const { username } = request.params;
 
-      const filteredAnimals = animals.map((animal) => ({
+      const teacher = await Teacher.findOne({ username }).populate("animals");
+
+      const filteredAnimals = teacher?.animals.map((animal) => ({
         name: animal.name,
         img: animal.img,
         selected: animal.selected,
@@ -20,14 +23,35 @@ class AnimalController {
 
   static async getSelectedAnimals(request: Request, response: Response) {
     try {
-      const animals = await Animal.find({ selected: true });
+      const { username } = request.params;
 
-      const filteredAnimals = animals.map((animal) => ({
+      const teacher = await Teacher.findOne({ username }).populate({
+        path: "animals",
+        match: { selected: { $eq: true } },
+      });
+
+      const filteredAnimals = teacher?.animals.map((animal) => ({
         name: animal.name,
         img: animal.img,
       }));
 
       response.status(200).send(filteredAnimals);
+    } catch (error: any) {
+      response.status(500).send({ error: "Error", message: error.message });
+    }
+  }
+
+  static async updateSelectedField(request: Request, response: Response) {
+    try {
+      const { username } = request.params;
+      const { name, state } = request.body;
+
+      const teacher = await Teacher.findOne({ username });
+      const a = await Animal.findOneAndUpdate(
+        { teacher, name },
+        { $set: { selected: state as boolean } }
+      );
+      response.status(200).send(a);
     } catch (error: any) {
       response.status(500).send({ error: "Error", message: error.message });
     }
